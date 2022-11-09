953c94f1503e2da4db7a9ff6b8ffd9591e08c406
coutinho21 <guilhermecscoutinho@gmail.com>
2022-11-09 12:54:06 +0000

    counted the chars in a file

--- src/Q1/samples.c
+++ src/Q1/samples.c
@@ -5,8 +5,12 @@
 int main(int argc, char *argv[]) {
   FILE *fp = fopen(argv[1], "r");
   int n = argv[2], m = argv[3];
-  
+  int charCount = 0;
+  setbuf(stdout, NULL); //buffer
+
   if (fp == NULL) printf("could not open file");
 
+  //count the number of chars in the file
+  for (char c = getc(fp); c != EOF; c = getc(fp)) charCount++;
   
 }
