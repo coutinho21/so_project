971b2716f9dce7b149d8cc1222a247f5dcab5223
MJBranco18 <mario.branco18@gmail.com>
2022-11-21 18:15:42 +0000

    Continued Q1

--- src/Q1/samples.c
+++ src/Q1/samples.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <libc.h>
+#include <string.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
-  FILE *fp = fopen(argv[1], "r");
-  int n = argv[2], m = argv[3];
-  int charCount = 0;
-  setbuf(stdout, NULL); //buffer
+    size_t t;
+    int fragmentNumber, sizeOfFragment;
+    FILE *fp = fopen(argv[1], "r");
 
-  if (fp == NULL) printf("could not open file");
+    fragmentNumber = atoi(argv[2]);
+    sizeOfFragment = atoi(argv[3]);
+    srand((unsigned) time(&t));
 
-  //count the number of chars in the file
-  for (char c = getc(fp); c != EOF; c = getc(fp)) charCount++;
-  
-  int randomNum;
-  unsigned int seed = 0;
-  for(int i = 0; i < n ; i++){
-    for(int j = 0 ; j < m ; j++){
-      //add random num here
+    if (fp == NULL){
+        perror("fopen():");
+        return EXIT_FAILURE;
     }
-  }
+    if(argc < 3){
+        printf("Number of arguments is smaller than expected.\n");
+        return EXIT_FAILURE;
+    }
+
+    if(fseek(fp, 0, SEEK_END) == -1){
+        perror("fseek():");
+        return EXIT_FAILURE;
+    }
+
+    int fileSize = ftell(fp);
+
+    if(fileSize == -1){
+        perror("ftell():");
+        return EXIT_FAILURE;
+    }
+
+    for(int i = 0; i < fragmentNumber ; i++){
+        int random = rand() % fileSize-sizeOfFragment;
+        char* buf = (char) malloc(sizeOfFragment+1(sizeof(char)));
+
+    }
+
 }
