c49913ebdc3c11848b4a9fc971ee2359cef3e1be
coutinho21 <guilhermecscoutinho@gmail.com>
2022-11-26 18:53:43 +0000

    Update on Q3 but still not creating the pipe properly

--- src/Q3/tokenring.c
+++ src/Q3/tokenring.c
@@ -8,7 +8,6 @@
 
 
 int main(int argc, char *argv[]){
-    printf("1 passei aqui");
     int n, t, val = 0, fd[2], mp, op;
     float p;
     char string[] = "pipeitoj";
@@ -20,48 +19,44 @@ int main(int argc, char *argv[]){
     for(int i = 1; i <= n; i++){
         string[4] = '0' + i;
         string[7] = '0' + i + 1;
-        printf("passei aqui 2");
+
         if(i == n){
             string[4] = '0' + n;
             string[7] = '0' + 1;
         }
-        printf("passei aqui");
+
         mp = mkfifo(string,0666);
-        if(mp > 0){
+        if(mp == -1){
             printf("error creating pipe\n");
             return EXIT_FAILURE;
         }
-        printf("passei aqui tambem");
+
+
         op = open(string, O_WRONLY);
         if(op == -1){
             printf("error occurred opening pipe\n");
             return EXIT_FAILURE;
         }
-        printf("tambem passei aqui");
-        int id = fork();
-        if(id == -1){
-            printf("error occurred creating child process\n");
+        //write
+        if(write(op, &val, sizeof(int)) == -1){
+            printf("error writing in the pipe\n");
             return EXIT_FAILURE;
         }
-        else if (id == 0){
-            close(fd[0]);
-            if(write(fd[1], &val, sizeof(int)) == -1){
-                printf("error writing in the pipe\n");
-                return EXIT_FAILURE;
-            }
-            close(fd[1]);
+        printf("sender sent the data.\n");
+        
+
+        op = open(string, O_RDONLY);
+        if(op == -1){
+            printf("error occurred opening pipe\n");
+            return EXIT_FAILURE;
         }
-        else if (id == 1){
-            close(fd[1]);
-            if(read(fd[0], &val, sizeof(int)) == -1){
-                printf("error reading from pipe\n");
-                return EXIT_FAILURE;
-            }
-            close(fd[0]);
-            val++;
-            printf("Got %d from child process.\n", val);
+        //read
+        if(read(op, &val, sizeof(int)) == -1){
+            printf("error reading from pipe\n");
+            return EXIT_FAILURE;
         }
-        printf("aqui tambem");
+        printf("Data received from sender: %d\n", val);
+        val++;
     }
 
     return 0;
