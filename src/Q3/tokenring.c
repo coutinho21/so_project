4b5100033bb439f5803d0b05120a1cfb79ee211a
coutinho21 <guilhermecscoutinho@gmail.com>
2022-12-01 00:22:49 +0000

    Added the rest of the makefiles and continued Q3

--- src/Q3/tokenring.c
+++ src/Q3/tokenring.c
@@ -8,7 +8,7 @@
 
 
 int main(int argc, char *argv[]){
-    int n, t, val = 0, fd[2], mp, op;
+    int n, t, val = 0, fd, mp, op;
     float p;
     char string[] = "pipeitoj";
     n = atoi(argv[1]);
@@ -24,42 +24,86 @@ int main(int argc, char *argv[]){
             string[4] = '0' + n;
             string[7] = '0' + 1;
         }
-
-        int flag = 0;
-
+        
         mp = mkfifo(string,0666);
+        
         if(mp == -1){
             printf("error creating pipe\n");
-            flag = 1;
+            return EXIT_FAILURE;
         }
+    }
 
-        if(!flag) op = open(string, O_WRONLY);
-        if(op == -1){
-            printf("error occurred opening pipe\n");
+    while(1){
+        fd = fork();
+        if(fd < 0){
+            printf("error creating process\n");
             return EXIT_FAILURE;
         }
 
-        //write
-        if(write(op, &val, sizeof(int)) == -1){
-            printf("error writing in the pipe\n");
-            return EXIT_FAILURE;
-        } else printf("sender sent the data.\n");
-        
-
-        if(!flag) op = open(string, O_RDONLY);
-        if(op == -1){
-            printf("error occurred opening pipe\n");
-            return EXIT_FAILURE;
+        //child process
+        if(fd == 0){
+            op = open(string, O_WRONLY);
+            if(op == -1){
+                printf("error opening pipe\n");
+                return EXIT_FAILURE;
+            }
+            write(op, &val, sizeof(int));
+            close(op);
+            printf("child %d wrote %d\n", getpid(), val);
+            
+            sleep(t);
+            exit(0);
         }
+        else{
+            //parent process
+            for(int i = 1; i <= n; i++){
+                string[4] = '0' + i;
+                string[7] = '0' + i + 1;
 
-        //read
-        if(read(op, &val, sizeof(int)) == -1){
-            printf("error reading from pipe\n");
-            return EXIT_FAILURE;
-        }else{
-            printf("Data received from sender: %d\n", val);
-            val++;
+                if(i == n){
+                    string[4] = '0' + n;
+                    string[7] = '0' + 1;
+                }
+                op = open(string, O_RDONLY);
+                if(op == -1){
+                    printf("error opening pipe\n");
+                    return EXIT_FAILURE;
+                }
+                read(op, &val, sizeof(int));
+                close(op);
+                printf("process %d: %d\n", i, val);
+                val++;
+            }
         }
+    }
+
+
+    op = open(string, O_WRONLY);
+    if(op == -1){
+        printf("error occurred opening pipe\n");
+        return EXIT_FAILURE;
+    }
+
+    //write
+    if(write(op, &val, sizeof(int)) == -1){
+        printf("error writing in the pipe\n");
+        return EXIT_FAILURE;
+    } else printf("sender sent the data.\n");
+    
+
+    op = open(string, O_RDONLY);
+    if(op == -1){
+        printf("error occurred opening pipe\n");
+        return EXIT_FAILURE;
+    }
+
+    //read
+    if(read(op, &val, sizeof(int)) == -1){
+        printf("error reading from pipe\n");
+        return EXIT_FAILURE;
+    }else{
+        printf("Data received from sender: %d\n", val);
+        val++;
     }
 
     return 0;
